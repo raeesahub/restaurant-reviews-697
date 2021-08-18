bdcf018d4eee99cd99933cef3a08660454685fa0
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-08-18 09:40:42 +0100

    add chef name to restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    collection do
+      get :top
+      # get 'restaurants/top', to: 'restaurants#top', as 'top_restaurants'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+100.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    stars: rand(1..5),
+    chef_name: ['Chef Boyarde', 'Gordon Ramsay', 'Marco Pierre-White', 'Felix The Cat', 'Remy'].sample
+  )
+  puts "Created restaurant with id #{restaurant.id}"
+end
