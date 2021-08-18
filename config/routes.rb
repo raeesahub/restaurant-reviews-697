2e89be7c197bab1bbc64bcc505392232b1ca67a9
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-08-18 11:19:31 +0100

    user can add and destroy a review

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
       get :top
       # get 'restaurants/top', to: 'restaurants#top', as 'top_restaurants'
     end
+    member do
+      get :chef
+    end
+    resources :reviews, only: [:new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources :reviews, only: [ :destroy ]
 end
